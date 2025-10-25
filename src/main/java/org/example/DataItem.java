695c68aee9541b5381447cb990db98d06c8d40d9
dreik84 <dreik84@mail.ru>
2025-10-25 15:59:57 +0300

    Create DataItem.java

---
+++ src/main/java/org/example/DataItem.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class DataItem {
+
+    private int iData;
+
+    public DataItem(int ii) {
+        iData = ii;
+    }
+
+    public int getKey() {
+        return iData;
+    }
+}
