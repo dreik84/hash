ed96ca0dca741062b260bffc04dd9fc5165bad7a
dreik84 <dreik84@mail.ru>
2025-10-30 16:16:56 +0300

    Create HashChain.java

---
+++ src/main/java/org/example/HashChain.java
@@ -0,0 +1,44 @@
+package org.example;
+
+// Реализация хеш-таблицы с использованием метода цепочек
+public class HashChain {
+    private SortedList[] hashArray;
+    private int arraySize;
+
+    public HashChain(int size) {
+        arraySize = size;
+        hashArray = new SortedList[arraySize];
+
+        for (int i = 0; i < arraySize; i++) {
+            hashArray[i] = new SortedList();
+        }
+    }
+
+    public void displayTable() {
+        for (int i = 0; i < arraySize; i++) {
+            System.out.print(i + " ");
+            hashArray[i].displayList();
+        }
+    }
+
+    public int hashFunc(int key) {
+        return key % arraySize;
+    }
+
+    public void insert(Link theLink) {
+        int key = theLink.getKey();
+        int hashVal = hashFunc(key);
+        hashArray[hashVal].insert(theLink);
+    }
+
+    public void delete(int key) {
+        int hashVal = hashFunc(key);
+        hashArray[hashVal].delete(key);
+    }
+
+    public Link find(int key) {
+        int hashVal = hashFunc(key);
+        Link theLink = hashArray[hashVal].find(key);
+        return theLink;
+    }
+}
